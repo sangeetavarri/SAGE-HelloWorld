08fa3b8cae0038d19c7caddeb526bb61efd4e59d
sangeetavarri <125152660+sangeetavarri@users.noreply.github.com>
2024-06-27 16:48:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+#Hello World Program 
+import numpy as np
+from waggle.plugin import Plugin
+import time
 
 def main():
-    print("Hello World");
-
+   with Plugin() as plugin:
+      print("Hello World")
+      t = time.time()
+      plugin.publish("hello_world_printed", t)
 
 if __name__ == "__main__":
     main()
